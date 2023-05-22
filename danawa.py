37f8feb456a9976ba86e71dfc78f4e1e5eabbe96
MinQyu <jorushs@gmail.com>
2023-05-22 20:00:24 +0900

    코드 대폭 수정

--- danawa.py
+++ danawa.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 from tkinter import *
 import tkinter as tk
@@ -14,7 +15,6 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://danawa.com")
 time.sleep(2)
-
 
 class App(tk.Tk):
     def __init__(self):
@@ -34,9 +34,7 @@ class App(tk.Tk):
             self._frame.destroy()
         self._frame = new_frame
         self._frame.pack(fill=BOTH)
-
-
-
+#검색페이지
 class StartPage(tk.Frame):
     def __init__(self, master):
         logo = tk.PhotoImage(file="logo.png")
@@ -45,15 +43,17 @@ class StartPage(tk.Frame):
         def Enter(event):
             Search()
         def Search(): #버튼 누를시 실행할 함수
-            검색어 = str(txt.get())
-            if(검색어 != ""):
+            search_word = str(txt.get())
+            if(search_word != ""):
                 searchBox = driver.find_element(By.CLASS_NAME,"search__input")
                 searchBox.click()
-                time.sleep(2)
-                searchBox.send_keys(검색어)
+                time.sleep(0.5)
+                searchBox.send_keys(search_word)
                 search = driver.find_element(By.CLASS_NAME, "search__submit")
                 search.click()
-                Search_list()
+                time.sleep(1)
+                scrolling()
+                search_list()
                 master.switch_frame(PageOne) #페이지 전환
             else:
                 msgbox.showinfo("알림", "검색어를 입력해주세요")
@@ -69,18 +69,30 @@ class StartPage(tk.Frame):
         btn.image = icon #가비지 컬렉터 삭제 방지
         btn.pack(side="right",padx=5)
 
-
 product_name=[]
 product_info=[]
 product_link=[]
 image_link=[]
 
 def list_clear():
-    product_name.clear
-    product_info.clear
-    image_link.clear
-    product_link.clear
-def Search_list():
+    product_name.clear()
+    product_info.clear()
+    image_link.clear()
+    product_link.clear()
+
+def scrolling():
+    lastHeight = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        
+        newHeight = driver.execute_script("return document.body.scrollHeight")
+        if newHeight == lastHeight:
+            break
+        lastHeight = newHeight
+
+
+
+#제품 가격 크롤링 함수            
+def search_list():
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     goods_list = soup.select('div.main_prodlist.main_prodlist_list > ul > li')
     tmp = "";
@@ -96,34 +108,48 @@ def Search_list():
         if img_link == None:
             img_link = v.select_one('div.thumb_image > a > img').get('src')
         if name != tmp:
-            product_name.append("제품명: "+name)
-            product_info.append("제품정보: "+prod_info)
-            product_link.append("제품링크: "+prod_link)
-            image_link.append(img_link)
+            product_name.append(name)
+            product_info.append(prod_info)
+            product_link.append(prod_link)
+            image_link.append("https:"+img_link)
             tmp = name
-
-
-
+    for i in range(len(image_link)):
+        print(image_link[i])
+        print('\n')
+#1페이지            
 class PageOne(tk.Frame):
     def __init__(self, master):
+        idx = -1;
         def Back():
             driver.get("https://danawa.com")
             master.switch_frame(StartPage)
             list_clear()
+        def event_for_listbox(event): 
+            w = event.widget
+            idx = int(w.curselection()[0])
+            Info_list = tk.Label(self, text=product_info[idx], height = 8, justify = LEFT ,wraplength = 560,anchor = SW, bg = "white", font=('맑은 고딕', 12, "bold")).place(x=0, y=470)
         tk.Frame.__init__(self, master)
         tk.Frame.configure(self,bg='white')
         scrollbar = tk.Scrollbar(self)
         scrollbar.pack(side="right", fill="y")
         Label_img = tk.Label(self, text="이미지", anchor = NW, bg = "white", font=('맑은 고딕', 18, "bold")).place(x=0, y=0)
         Label_info = tk.Label(self, text="제품 정보", height = 5, anchor = SW, bg = "white", font=('맑은 고딕', 18, "bold")).place(x=0, y=250)
-        tk.Button(self, text="Back", command=Back).place(x=0, y=670)
-        tk.Button(self, text="Select", command=NONE).place(x=50, y=670)
-        Listbox = tk.Listbox(self, bg = 'white', width = -5, height = 0, justify=LEFT, font=('맑은 고딕',12,"bold"),xscrollcommand=scrollbar.set, yscrollcommand=scrollbar.set)
+        Listbox = tk.Listbox(self, bg='white', width=70, height = 0, justify=LEFT, selectbackground="chartreuse3", highlightcolor="lightgreen", highlightthickness=2, activestyle="none", font=('맑은 고딕',12,"bold"),yscrollcommand=scrollbar.set)
         for i in range(len(product_name)):
             Listbox.insert(i,product_name[i])
         Listbox.pack(side="right", fill=BOTH, padx=0)
-        scrollbar["command"]=Listbox.yview
+        Listbox.bind('<<ListboxSelect>>', event_for_listbox)
+        #다음 페이지로 넘어가는 함수
+        def Next():
+            if idx == -1:
+                msgbox.showinfo("알림", "제품을 선택해주세요")
+            else:
+                driver.get(product_link[idx])
+                master.switch_frame(PageTwo)
 
+        tk.Button(self, text="Back", command=Back).place(x=0, y=670)
+        tk.Button(self, text="Select", command=Next).place(x=50, y=670)
+        scrollbar["command"]=Listbox.yview
 
 class PageTwo(tk.Frame):
     def __init__(self, master):
@@ -132,14 +158,6 @@ class PageTwo(tk.Frame):
         tk.Label(self, text="Page two", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
         tk.Button(self, text="Go back to start page", command=lambda: master.switch_frame(StartPage)).pack()
 
-
 if __name__ == "__main__":
     app = App()
     app.mainloop()
-
-
-
-
-
-
-
