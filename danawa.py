f946be433fc37c999df1b3584699bb92e15b4149
LEESJ7934 <103932107+LEESJ7934@users.noreply.github.com>
2023-05-22 21:50:41 +0900

    사진 출력 추가

--- danawa.py
+++ danawa.py
@@ -2,9 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 from tkinter import *
 import tkinter as tk
+from io import BytesIO
+from PIL import Image, ImageTk
+from urllib.request import urlopen
 import tkinter.messagebox as msgbox
 import tkinter.font
 import time
@@ -42,15 +46,17 @@ class StartPage(tk.Frame):
         def Enter(event):
             Search()
         def Search(): #버튼 누를시 실행할 함수
-            검색어 = str(txt.get())
-            if(검색어 != ""):
+            search_word = str(txt.get())
+            if(search_word != ""):
                 searchBox = driver.find_element(By.CLASS_NAME,"search__input")
                 searchBox.click()
-                time.sleep(2)
-                searchBox.send_keys(검색어)
+                time.sleep(0.5)
+                searchBox.send_keys(search_word)
                 search = driver.find_element(By.CLASS_NAME, "search__submit")
                 search.click()
-                Search_list()
+                time.sleep(1)
+                scrolling()
+                search_list()
                 master.switch_frame(PageOne) #페이지 전환
             else:
                 msgbox.showinfo("알림", "검색어를 입력해주세요")
@@ -72,36 +78,22 @@ product_link=[]
 image_link=[]
 
 def list_clear():
-    product_name.clear
-    product_info.clear
-    image_link.clear
-    product_link.clear
+    product_name.clear()
+    product_info.clear()
+    image_link.clear()
+    product_link.clear()
 
-#제품 정보 크롤링 함수
-def Search_list():
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    goods_list = soup.select('div.main_prodlist.main_prodlist_list > ul > li')
-    tmp = "";
-    for v in goods_list:
-        new_text = ""
-        if v.find('div', class_='prod_main_info'):
-            name = v.select_one('p.prod_name > a').text.strip()
-            prod_info = ''  # 제품스펙
-            for s in v.select('div.spec_list > a'):
-                prod_info += s.text + '/'
-            prod_link = v.select_one('p.prod_name > a')['href']
-            img_link = v.select_one('div.thumb_image > a > img').get('data-original')
-        if img_link == None:
-            img_link = v.select_one('div.thumb_image > a > img').get('src')
-        if name != tmp:
-            product_name.append("제품명: "+name)
-            product_info.append(prod_info)
-            product_link.append(prod_link)
-            image_link.append(img_link)
-            tmp = name
+
+
+def scrolling():
+    element=driver.find_element(By.TAG_NAME,"html")
+    for i in range(30):
+        element.send_keys(Keys.SPACE)
+   
+idx = -1
 
 #제품 가격 크롤링 함수            
-def Search_list():
+def search_list():
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     goods_list = soup.select('div.main_prodlist.main_prodlist_list > ul > li')
     tmp = "";
@@ -117,42 +109,51 @@ def Search_list():
         if img_link == None:
             img_link = v.select_one('div.thumb_image > a > img').get('src')
         if name != tmp:
-            product_name.append("제품명: "+name)
+            product_name.append(name)
             product_info.append(prod_info)
             product_link.append(prod_link)
-            image_link.append(img_link)
+            image_link.append("https:"+img_link)
             tmp = name
+    for i in range(len(image_link)):
+        print(image_link[i])
+        print('\n')
 #1페이지            
 class PageOne(tk.Frame):
     def __init__(self, master):
         def Back():
             driver.get("https://danawa.com")
             master.switch_frame(StartPage)
-            list_clear()   
+            list_clear()
         def event_for_listbox(event): 
+            global idx
             w = event.widget
-            check = int(w.curselection()[0])
-            Info_list = tk.Label(self, text=product_info[check], height = 8, justify = LEFT ,wraplength = 560,anchor = SW, bg = "white", font=('맑은 고딕', 12, "bold")).place(x=0, y=470)
+            idx = int(w.curselection()[0])
+            URL = image_link[idx]
+            u = urlopen(URL)
+            raw_data = u.read()
+            im = Image.open(BytesIO(raw_data))
+            photo = ImageTk.PhotoImage(im)
+            label = tk.Label(image=photo, bg = "white", width = 150, height = 150).place(x = 20, y = 50)
+            Info_list = tk.Label(self, text=product_info[idx], height = 8, justify = LEFT ,wraplength = 560,anchor = SW, bg = "white", font=('맑은 고딕', 12, "bold")).place(x=0, y=470)
+            label.image = photo
         tk.Frame.__init__(self, master)
         tk.Frame.configure(self,bg='white')
         scrollbar = tk.Scrollbar(self)
         scrollbar.pack(side="right", fill="y")
         Label_img = tk.Label(self, text="이미지", anchor = NW, bg = "white", font=('맑은 고딕', 18, "bold")).place(x=0, y=0)
         Label_info = tk.Label(self, text="제품 정보", height = 5, anchor = SW, bg = "white", font=('맑은 고딕', 18, "bold")).place(x=0, y=250)
-        Listbox = tk.Listbox(self, bg = 'white', width = 70, height = 0, justify= LEFT, font=('맑은 고딕',12,"bold"),yscrollcommand=scrollbar.set)
-        for i in range(len(product_name)):
+        Listbox = tk.Listbox(self, bg='white', width=70, height = 0, justify=LEFT, selectbackground="chartreuse3", highlightcolor="lightgreen", highlightthickness=2, activestyle="none", font=('맑은 고딕',12,"bold"),yscrollcommand=scrollbar.set)
+        for i in range(40):
             Listbox.insert(i,product_name[i])
         Listbox.pack(side="right", fill=BOTH, padx=0)
         Listbox.bind('<<ListboxSelect>>', event_for_listbox)
-        #선택한 목록의 index 값을 출력 하는 함수, tuple을 정수형으로 바꿔서 숫자 값만 가져옴
-        def command_return_index():
-            res = int(''.join(map(str, Listbox.curselection())))
-            return res
         #다음 페이지로 넘어가는 함수
         def Next():
-            link = command_return_index()
-            driver.get(product_link[link])
-            master.switch_frame(PageTwo)
+            if idx == -1:
+                msgbox.showinfo("알림", "제품을 선택해주세요")
+            else:
+                driver.get(product_link[idx])
+                master.switch_frame(PageTwo)
 
         tk.Button(self, text="Back", command=Back).place(x=0, y=670)
         tk.Button(self, text="Select", command=Next).place(x=50, y=670)
@@ -160,7 +161,6 @@ class PageOne(tk.Frame):
 
 class PageTwo(tk.Frame):
     def __init__(self, master):
-        
         tk.Frame.__init__(self, master)
         tk.Frame.configure(self,bg='red')
         tk.Label(self, text="Page two", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
