6b1f17ba66c4484393326e3f2a694eca3358c2a1
LEESJ7934 <103932107+LEESJ7934@users.noreply.github.com>
2023-05-24 22:57:44 +0900

    4사마켓 상품번호 출력, 상품번호에 맞는 사이트 이동 추가

--- danawa.py
+++ danawa.py
@@ -1,22 +1,25 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 from tkinter import *
-import tkinter as tk
 from io import BytesIO
 from PIL import Image, ImageTk
 from urllib.request import urlopen
+import tkinter as tk
 import tkinter.messagebox as msgbox
 import tkinter.font
 import time
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options) #상품 검색 홈페이지
 driver.get("https://danawa.com")
+Price_driver = webdriver.Chrome(options=chrome_options)  #4사 검색 홈페이지
+
 time.sleep(2)
 
 class App(tk.Tk):
@@ -31,12 +34,15 @@ class App(tk.Tk):
         self._frame = None
         self.switch_frame(StartPage)
 
-    def switch_frame(self, frame_class):
+    def switch_frame(self, frame_class): #페이지 전환 함수
         new_frame = frame_class(self)
         if self._frame is not None:
             self._frame.destroy()
         self._frame = new_frame
         self._frame.pack(fill=BOTH)
+
+
+
 #검색페이지
 class StartPage(tk.Frame):
     def __init__(self, master):
@@ -72,93 +78,226 @@ class StartPage(tk.Frame):
         btn.image = icon #가비지 컬렉터 삭제 방지
         btn.pack(side="right",padx=5)
 
+#상품 정보 리스트 초기화
 product_name=[]
 product_info=[]
 product_link=[]
 image_link=[]
+idx = -1 # 리스트박스 인덱스
 
 def list_clear():
+    global idx
     product_name.clear()
     product_info.clear()
     image_link.clear()
     product_link.clear()
-
-
+    idx = -1
 
 def scrolling():
     element=driver.find_element(By.TAG_NAME,"html")
     for i in range(30):
         element.send_keys(Keys.SPACE)
-   
-idx = -1
 
 #제품 가격 크롤링 함수            
 def search_list():
+    global product_info
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     goods_list = soup.select('div.main_prodlist.main_prodlist_list > ul > li')
-    tmp = "";
+    tmp = ""
     for v in goods_list:
-        new_text = ""
         if v.find('div', class_='prod_main_info'):
-            name = v.select_one('p.prod_name > a').text.strip()
-            prod_info = ''  # 제품스펙
-            for s in v.select('div.spec_list > a'):
-                prod_info += s.text + '/'
+            name = v.select_one('p.prod_name > a').text.strip()  
             prod_link = v.select_one('p.prod_name > a')['href']
             img_link = v.select_one('div.thumb_image > a > img').get('data-original')
         if img_link == None:
             img_link = v.select_one('div.thumb_image > a > img').get('src')
         if name != tmp:
             product_name.append(name)
-            product_info.append(prod_info)
             product_link.append(prod_link)
             image_link.append("https:"+img_link)
             tmp = name
-    for i in range(len(image_link)):
-        print(image_link[i])
-        print('\n')
+    product_info = driver.find_elements(By.CLASS_NAME, "spec_list")
+
+
+
 #1페이지            
 class PageOne(tk.Frame):
     def __init__(self, master):
-        def Back():
+        def Back(): #이전 페이지
             driver.get("https://danawa.com")
             master.switch_frame(StartPage)
             list_clear()
-        def event_for_listbox(event): 
-            global idx
-            w = event.widget
-            idx = int(w.curselection()[0])
-            URL = image_link[idx]
-            u = urlopen(URL)
-            raw_data = u.read()
-            im = Image.open(BytesIO(raw_data))
-            photo = ImageTk.PhotoImage(im)
-            label = tk.Label(image=photo, bg = "white", width = 150, height = 150).place(x = 20, y = 50)
-            Info_list = tk.Label(self, text=product_info[idx], height = 8, justify = LEFT ,wraplength = 560,anchor = SW, bg = "white", font=('맑은 고딕', 12, "bold")).place(x=0, y=470)
-            label.image = photo
-        tk.Frame.__init__(self, master)
-        tk.Frame.configure(self,bg='white')
-        scrollbar = tk.Scrollbar(self)
-        scrollbar.pack(side="right", fill="y")
-        Label_img = tk.Label(self, text="이미지", anchor = NW, bg = "white", font=('맑은 고딕', 18, "bold")).place(x=0, y=0)
-        Label_info = tk.Label(self, text="제품 정보", height = 5, anchor = SW, bg = "white", font=('맑은 고딕', 18, "bold")).place(x=0, y=250)
-        Listbox = tk.Listbox(self, bg='white', width=70, height = 0, justify=LEFT, selectbackground="chartreuse3", highlightcolor="lightgreen", highlightthickness=2, activestyle="none", font=('맑은 고딕',12,"bold"),yscrollcommand=scrollbar.set)
-        for i in range(40):
-            Listbox.insert(i,product_name[i])
-        Listbox.pack(side="right", fill=BOTH, padx=0)
-        Listbox.bind('<<ListboxSelect>>', event_for_listbox)
-        #다음 페이지로 넘어가는 함수
-        def Next():
+            
+
+
+        def Next():#다음 페이지로 넘어가는 함수
             if idx == -1:
                 msgbox.showinfo("알림", "제품을 선택해주세요")
             else:
                 driver.get(product_link[idx])
+                CoupangSearch()
+                driver.implicitly_wait(10)
+                GmarketSearch()
+                driver.implicitly_wait(10)
+                AuctionSearch()
+                driver.implicitly_wait(10)
+                EleSearch()
                 master.switch_frame(PageTwo)
 
-        tk.Button(self, text="Back", command=Back).place(x=0, y=670)
-        tk.Button(self, text="Select", command=Next).place(x=50, y=670)
-        scrollbar["command"]=Listbox.yview
 
+
+        def event_for_listbox(event): #리스트박스 항목 클릭시
+            global idx
+            w = event.widget
+            idx = int(w.curselection()[0])
+            label_info.config(text=product_info[idx].text)
+            u = urlopen(image_link[idx])
+            raw_data = u.read()
+            u.close()
+            img = Image.open(BytesIO(raw_data))
+            img_resize = img.resize((200,200))
+            photo = ImageTk.PhotoImage(img_resize)
+            label_img.config(image=photo)
+            label_img.image = photo #가비지 컬렉터 삭제 방지
+
+
+        #4사 마켓 가격 크롤링 함수
+        def CoupangSearch(): 
+            driver.implicitly_wait(10)
+            Cou_number = Coupang_number()
+            if Cou_number == None: #해당마켓에 상품 존재하지 않을 시 함수 종료
+                return  
+            driver.implicitly_wait(10)
+            Price_driver.get("https://www.coupang.com/vp/products/" + Cou_number)
+
+        def GmarketSearch(): 
+            driver.implicitly_wait(10)
+            Gmk_number = Gmarket_number()
+            if Gmk_number == None:
+                return
+            driver.implicitly_wait(10)
+            Price_driver.get("https://item.gmarket.co.kr/Item?goodscode=" + Gmk_number)
+
+        def AuctionSearch(): 
+            driver.implicitly_wait(10)
+            Auc_number = Auction_number()
+            if Auc_number == None:
+                return
+            driver.implicitly_wait(10)
+            Price_driver.get("http://itempage3.auction.co.kr/DetailView.aspx?itemno=" + Auc_number)
+
+        def EleSearch(): 
+            driver.implicitly_wait(10)
+            E_number = Ele_number()
+            if E_number == None:
+                return
+            driver.implicitly_wait(10)
+            Price_driver.get("https://www.11st.co.kr/products/" + E_number)
+
+
+        #4사 마켓 상품 번호 크롤링 함수
+        def Gmarket_number():
+            driver.get(product_link[idx])
+            try: 
+                Gmarket_link = driver.find_element(By.XPATH, '//img[@alt="G마켓"]')
+                Gmarket_link.click()
+            except NoSuchElementException: 
+                print("제품이 존재하지 않습니다.")
+            else:
+                driver.switch_to.window(driver.window_handles[1])
+                driver.implicitly_wait(10)
+                Gmarket_url = driver.current_url
+                First_index= Gmarket_url.find("link_pcode")
+                Last_index = Gmarket_url.find("&package")
+                driver.implicitly_wait(10)
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+                driver.implicitly_wait(10)
+                Gmarket_prodnumber = Gmarket_url[First_index+len("link_pcode")+1:Last_index]
+                return Gmarket_prodnumber
+
+        def Auction_number():
+            driver.get(product_link[idx])
+            try: 
+                Auction_link = driver.find_element(By.XPATH, '//img[@alt="옥션"]')
+                Auction_link.click()
+            except NoSuchElementException: 
+                print("제품이 존재하지 않습니다.")
+            else:
+                driver.switch_to.window(driver.window_handles[1])
+                driver.implicitly_wait(10)
+                Auction_url = driver.current_url
+                First_index= Auction_url.find("link_pcode")
+                Last_index = Auction_url.find("&package")
+                driver.implicitly_wait(10)
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+                driver.implicitly_wait(10)
+                Auction_prodnumber = Auction_url[First_index+len("link_pcode")+1:Last_index]
+                return Auction_prodnumber
+
+        def Coupang_number():
+            try: 
+                Coupang_link = driver.find_element(By.XPATH, '//img[@alt="쿠팡"]')
+                Coupang_link.click()
+            except NoSuchElementException: 
+                print("제품이 존재하지 않습니다.")
+            else:
+                driver.switch_to.window(driver.window_handles[1])
+                driver.implicitly_wait(10)
+                Coupang_url = driver.current_url
+                First_index= Coupang_url.find("link_pcode")
+                Last_index = Coupang_url.find("&package")
+                driver.implicitly_wait(10)
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+                driver.implicitly_wait(10)
+                Coupang_prodnumber = Coupang_url[First_index+len("link_pcode")+2:Last_index]
+                return Coupang_prodnumber
+            
+        def Ele_number():
+            driver.get(product_link[idx])
+            try: 
+                Ele_link = driver.find_element(By.XPATH, '//img[@alt="11번가"]')
+                Ele_link.click()
+            except NoSuchElementException: 
+                print("제품이 존재하지 않습니다.")
+            else:
+                driver.switch_to.window(driver.window_handles[1])
+                driver.implicitly_wait(10)
+                Ele_url = driver.current_url
+                First_index= Ele_url.find("link_pcode")
+                Last_index = Ele_url.find("&package") 
+                driver.implicitly_wait(10)
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+                driver.implicitly_wait(10)
+                Ele_pronumber = Ele_url[First_index+len("link_pcode")+1:Last_index]
+                return Ele_pronumber    
+            
+
+
+        tk.Frame.__init__(self, master)
+        tk.Frame.configure(self, bg='white')
+        scrollbar = tk.Scrollbar(self)
+        scrollbar.pack(side="right", fill="y")
+        tk.Label(self, text="이미지", anchor = NW, bg = "white", font=('맑은 고딕', 15, "bold")).place(x=5, y=0)
+        tk.Label(self, text="제품 정보", anchor = SW, bg = "white", font=('맑은 고딕', 15, "bold")).place(x=5, y=320)
+        label_info= tk.Label(self, text="상품을 선택 후 select 버튼을 눌러주세요", height = 15, width = 60, highlightthickness=2, highlightbackground="lightgreen",
+                             relief="groove", justify = LEFT ,wraplength = 600, anchor = NW, bg = "white", font=('맑은 고딕', 12, "bold"))
+        label_info.place(x=5, y=350)
+        label_img = tk.Label(self, bg = "white")
+        label_img.place(x = 200, y = 60)
+        Listbox = tk.Listbox(self, bg='white', width=70, height = 0, justify=LEFT, selectbackground="chartreuse3",
+                             highlightbackground="lightgreen", highlightcolor="lightgreen", highlightthickness=2, activestyle="none", font=('맑은 고딕',12,"bold"),yscrollcommand=scrollbar.set)
+        for i in range(40):
+            Listbox.insert(i,product_name[i])
+        Listbox.pack(side="right", fill=BOTH, padx=0)
+        Listbox.bind('<<ListboxSelect>>', event_for_listbox)
+        tk.Button(self, text="Back", command=Back).place(x=25, y=685)
+        tk.Button(self, text="Select", command=Next).place(x=85, y=685)
+        scrollbar["command"]=Listbox.yview
+        
 class PageTwo(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
